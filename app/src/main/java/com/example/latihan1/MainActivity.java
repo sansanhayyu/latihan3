df225415ec1b9d156ead038eb2993a73f1b31183
sansanhayyu@gmail.com <sansanhayyu@gmali.com>
2021-10-17 19:08:04 +0700

    penambahan button save

---
+++ app/src/main/java/com/example/latihan1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.latihan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
+    }
+}
